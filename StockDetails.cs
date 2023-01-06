2fd2d2eedfe4340e2a141652a3beecf623a28c4b
“Salil48” <salilchharia@gmail.com>
2023-01-06 18:28:50 +0530

    Stocks Management

---
+++ StockDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+    public class StockDetails
+    {
+        public List<StocksList> stocksList;
+    }
+
+    public class StocksList
+    {
+        public string StockName;
+        public int Shares;
+        public int Price;
+    }
+}
